19eb34b3676ad8aab58d9a7ad74642ee39df3f3f
Rob Ranf <rob@emiyaconsulting.com>
2023-04-13 17:32:15 -0700

    adding edit page and logic

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,77 +1,58 @@
 <script setup>
-	import { ref, computed } from "vue";
-	import SidebarLayout from "@/layouts/SidebarLayout.vue";
-	import CafeCard from "@/components/CafeCard.vue";
-	import BaseCheckbox from "@/components/base/BaseCheckbox.vue";
-	import BaseContainer from "@/components/base/BaseContainer.vue";
-	import BaseForm from "@/components/base/BaseForm.vue";
-	import BaseInput from "@/components/base/BaseInput.vue";
-	import { useFirestore } from "vuefire";
-	import {
-		collection,
-		query,
-		where,
-		getDocs,
-		onSnapshot,
-	} from "firebase/firestore";
+import { ref, computed } from 'vue'
+import SidebarLayout from '@/layouts/SidebarLayout.vue'
+import CafeCard from '@/components/CafeCard.vue'
+import BaseCheckbox from '@/components/base/BaseCheckbox.vue'
+import BaseContainer from '@/components/base/BaseContainer.vue'
+import BaseForm from '@/components/base/BaseForm.vue'
+import BaseInput from '@/components/base/BaseInput.vue'
 
-	const db = useFirestore();
+import { useFirestore, useCollection } from 'vuefire'
+import { collection } from 'firebase/firestore'
 
-	const q = query(collection(db, "cafes"), where("isActive", "==", true));
-	const cafeCollection = ref([]);
-	const querySnapshot = await getDocs(q);
+const db = useFirestore()
 
-	onSnapshot(q, () => {
-		cafeCollection.value = [];
-		querySnapshot.forEach((doc) => {
-			// doc.data() is never undefined for query doc snapshots
-			console.log(`ID: ${doc.id}`, " => ", doc.data());
-			cafeCollection.value.push(doc.data());
-		});
-	});
+const cafeCollection = useCollection(collection(db, 'cafes'))
 
-	const filterParams = ref({
-		text: "",
-		favorite: false,
-	});
+const filterParams = ref({
+  text: '',
+  favorite: false,
+})
 
-	const filteredCafes = computed(() => {
-		return cafeCollection.value.filter((cafe) => {
-			return (
-				cafe.name
-					.toLowerCase()
-					.includes(filterParams.value.text.toLowerCase()) &&
-				(filterParams.value.favorite ? cafe.favorite : true)
-			);
-		});
-	});
+const filteredCafes = computed(() => {
+  return cafeCollection.value.filter((cafe) => {
+    return (
+      cafe.name.toLowerCase().includes(filterParams.value.text.toLowerCase()) &&
+      (filterParams.value.favorite ? cafe.favorite : true)
+    )
+  })
+})
 </script>
 
 <template>
-	<SidebarLayout>
-		<template v-slot:sidebar>
-			<BaseContainer>
-				<h2 class="mb-4">Filter</h2>
-				<BaseForm>
-					<BaseInput v-model="filterParams.text" label="Name" />
-					<BaseCheckbox
-						v-model="filterParams.favorite"
-						label="Favorited" />
-				</BaseForm>
-			</BaseContainer>
-		</template>
+  <SidebarLayout>
+    <template v-slot:sidebar>
+      <BaseContainer>
+        <h2 class="mb-4">Filter</h2>
+        <BaseForm>
+          <BaseInput v-model="filterParams.text" label="Name" />
+          <BaseCheckbox v-model="filterParams.favorite" label="Favorited" />
+        </BaseForm>
+      </BaseContainer>
+    </template>
 
-		<template v-slot:main>
-			<BaseContainer>
-				<h2 class="mb-4">Content</h2>
-				<CafeCard
-					v-for="cafe in filteredCafes"
-					v-bind="cafe"
-					:docId="cafe.id"
-					:key="cafe.id" />
-			</BaseContainer>
-		</template>
-	</SidebarLayout>
+    <template v-slot:main>
+      <BaseContainer>
+        <h2 class="mb-4">Content</h2>
+        <CafeCard
+          v-for="cafe in filteredCafes"
+          v-bind="cafe"
+          :docId="cafe.id"
+          :key="cafe.id"
+        />
+      </BaseContainer>
+    </template>
+  </SidebarLayout>
 </template>
 
 <style></style>
